3c1fccc99457980f121cdc810f9ca96951a3e775
mattbutlar <matt.butlar@mindbodyonline.com>
2012-07-25 16:37:17 -0700

    C# Example

---
+++ C#/Default.aspx
@@ -0,0 +1,9 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="API_Example._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h1>All Classes</h1>
+    <p><asp:label ID="Classes" runat="server"></asp:label></p>
+</asp:Content>
---
+++ C#/Default.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace API_Example
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Set credential values
+            string sourcename = "YourSourceName";
+            string password = "YourPassword";
+            int[] siteIDs = {-99};
+
+            // Setup Service
+            ClassService.ClassService service = new ClassService.ClassService();
+
+            // Create and fill request
+            ClassService.GetClassesRequest request = new ClassService.GetClassesRequest();
+
+            request.SourceCredentials = new ClassService.SourceCredentials();
+            request.SourceCredentials.SourceName = sourcename;
+            request.SourceCredentials.Password = password;
+            request.SourceCredentials.SiteIDs = siteIDs;
+
+            //Run call with request and fill result  
+            ClassService.GetClassesResult result = service.GetClasses(request);
+            
+            //Display result in label
+            foreach(ClassService.Class thisClass in result.Classes)
+            {
+                Classes.Text += "<br/>______________________________________<div><p>" + 
+                    thisClass.ClassDescription.Name + "</p>" +
+                    "<p>" + thisClass.ID + "</p>" +
+                    "<p>" + thisClass.EndDateTime + " - " + thisClass.EndDateTime + "</p>" +
+                    "<p>" + thisClass.ClassDescription.Description + "</p></div>";
+            }
+        }
+    }
+}
